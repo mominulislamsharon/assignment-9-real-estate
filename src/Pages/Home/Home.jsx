bb2c7141d102535442b6ecb9d7e01d57603536d8
mominulislam2397 <islammominul3378@gmail.com>
2024-04-08 01:21:34 +0600

    setup react router and tailwind and daisy ui creat folder

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-2xl">This is Home Page...</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      }
+    ]
+  },
+]);
+
+export default router;
