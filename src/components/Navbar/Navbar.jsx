2d7f5263df88f8fada9c30ab464ef421e071e7db
mominulislam2397 <islammominul3378@gmail.com>
2024-04-16 21:40:32 +0600

    added fake data json

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     })
   }
   return (
-    <div className="navbar container mx-auto">
+    <div className="navbar fixed z-10 bg-white">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
