5b725e91ff6a98e1386998eaf51f09fe8c6d6dfa
mominulislam2397 <islammominul3378@gmail.com>
2024-04-18 05:49:56 +0600

    crate footer

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import OfficeBulding from "../../components/OfficeBulding/OfficeBulding";
-
+import BannerSlider from "../../components/BannerSlider/BannerSlider";
 const Home = () => {
   const [officeBulding, setOfficeBulding] = useState([]);
   
@@ -12,9 +12,10 @@ const Home = () => {
   },[])
   return (
     <div>
-      <h1 className=' text-black text-5xl'>this is slider </h1>
+      <BannerSlider></BannerSlider>
       <div>
-          <h1 className="text-center mt-7 text-3xl font-bold">Office Building</h1>
+          <h1 className="text-center mt-7 text-3xl font-bold">Recent Property Listed</h1>
+          <p className="text-center text-slate-500 font-medium pt-2">Unlock your potential, seize opportunities, embrace challenges. Fuel your passion and inspire greatness within yourself</p>
           <div className="mt-6 grid grid-cols-3 gap-6">
           {
             officeBulding.map(office => <OfficeBulding key={office.id} office={office}></OfficeBulding>)
--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Home from "../Pages/Home/Home";
+import Footer from "../components/Footer/Footer";
 
 const Root = () => {
   return (
     <div className="container mx-auto">
       <Navbar></Navbar>
      <Outlet></Outlet>
+     <Footer></Footer>
     </div>
   );
 };
