bfbb6f176630928a67eb4bd3a744465b41d84b53
mominulislam2397 <islammominul3378@gmail.com>
2024-04-19 20:59:48 +0600

    added home page animation and error page

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
-import Home from "../Pages/Home/Home";
 import Footer from "../components/Footer/Footer";
 
 const Root = () => {
