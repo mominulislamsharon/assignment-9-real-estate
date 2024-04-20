1a40d65f92ea4d9d57de48b976455d770b54dc44
mominulislam2397 <islammominul3378@gmail.com>
2024-04-20 12:28:16 +0600

    added animation and error page

--- src/components/BannerSlider/BannerSlider.jsx
+++ src/components/BannerSlider/BannerSlider.jsx
@@ -56,7 +56,7 @@ const BannerSlider = () => {
     </div>
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
+      <a href="#slide5" className="btn btn-circle">❯</a>
     </div>
   </div>
   <div id="slide5" className="carousel-item relative w-full">
@@ -69,8 +69,8 @@ const BannerSlider = () => {
     </div>
     </div>
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
+      <a href="#slide5" className="btn btn-circle">❮</a> 
+      <a href="#slide6" className="btn btn-circle">❯</a>
     </div>
   </div>
   <div id="slide6" className="carousel-item relative w-full">
@@ -83,7 +83,7 @@ const BannerSlider = () => {
     </div>
     </div>
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide6" className="btn btn-circle">❮</a> 
+      <a href="#slide5" className="btn btn-circle">❮</a> 
       <a href="#slide1" className="btn btn-circle">❯</a>
     </div>
   </div>
